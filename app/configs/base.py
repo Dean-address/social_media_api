8b23273b1cfc94ccc7b1800bca2ecb402000c814
Alabi Khalid <alabikhalid.12@gmail.com>
2025-01-01 12:14:43 +0100

    fix djoser error of not able to provid data of the  authenticated user

--- app/configs/base.py
+++ app/configs/base.py
@@ -22,6 +22,8 @@ LOCAL_APP = [
 
 THIRD_PARTY_APP = [
     "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt.token_blacklist",
     "djoser",
     "corsheaders",
     "drf_yasg",
@@ -110,14 +112,17 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
 
-REST_FRAMWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
+
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("JWT",),
+    "AUTH_HEADER_TYPES": ("Bearer",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
